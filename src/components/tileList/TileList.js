01f3a290f348f63750e699b9834eaac2f10031c1
Vy Nguyen <vydpnguyen@gmail.com>
2023-08-01 20:48:12 +0700

    Implement Tile as stateless component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {tiles.map((tile, index) => {
+        // Extract name property and description
+        const {name, ...description} = tile;
+        // return Tile component
+        return <Tile name={name} description={description} key={index} />
+      })}
     </div>
   );
 };
